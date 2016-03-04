18989768198117a262707f74ae10d9848991b427
Joe Miller <joemillervi@gmail.com>
2016-03-04 11:42:38 -0800

    Re add checkUserExists function and fix bitcoin model

--- server/models/users.js
+++ server/models/users.js
@@ -5,18 +5,26 @@ var Users = function() {
 };
 
 Users.prototype.saveCCPaymentId = function (custId, githubId) {
-  return db('users').where({github_id: githubId})
-                    .update({stripe_cust_id: custId})
+  return db('users')
+  .where({github_id: githubId})
+  .update({stripe_cust_id: custId})
+};
+
+// check if user exists
+Users.prototype.doesUserExist = function (githubId) {
+  return db('users')
+  .where({github_id: githubId})
 };
 
 Users.prototype.saveBankRecipientId = function (recipientName, recipientType, recipientId, recipientEmail, githubId) {
-  return db('users').where({github_id: githubId})
-                    .update({
-                    	stripe_recipient_name: recipientId,
-                    	stripe_recipient_type: recipientId,
-                    	stripe_recipient_id: recipientId,
-                    	stripe_recipient_email: recipientId
-                    })
+  return db('users')
+  .where({github_id: githubId})
+  .update({
+  	stripe_recipient_name: recipientId,
+  	stripe_recipient_type: recipientId,
+  	stripe_recipient_id: recipientId,
+  	stripe_recipient_email: recipientId
+  })
 };
 
 Users.prototype.createUser = function (customer) {
