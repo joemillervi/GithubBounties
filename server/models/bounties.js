ab874093c32868044e6bd6ee2d83b63748534d2f
Joe Miller <joemillervi@gmail.com>
2016-03-04 11:34:26 -0800

    Fix bounties model

--- server/models/bounties.js
+++ server/models/bounties.js
@@ -8,20 +8,12 @@ Bounties.prototype.saveBitcoin = function (bitCoinAmount, org_name, repo_name, n
   return db('bountyIssues')
     .where({github_id: githubId})
     .update({
-    	bitcoin_amout: recipientId,
-    	stripe_recipient_type: recipientId,
-    	stripe_recipient_id: recipientId,
-    	stripe_recipient_email: recipientId
+    	bitcoin_amout: bitCoinAmount,
+    	org_name: org_name,
+    	repo_name: repo_name,
+    	number: number,
+      githubId: githubId
     })
-};
-
-Bounties.prototype.createUser = function (customer) {
-  return db('users').insert({
-    github_id: customer.id,
-    github_login: customer.login,
-    github_name: customer.name,
-    github_email: customer.email
-  })
 };
 
 module.exports = Bounties;
